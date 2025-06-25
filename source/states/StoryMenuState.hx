901019778a44fb7c0dbce6dff74f549453a7756e
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-06-25 20:12:13 +0300

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -52,10 +52,10 @@ class StoryMenuState extends MusicBeatState
 		persistentUpdate = persistentDraw = true;
 
 		scoreText = new FlxText(10, 10, 0, "SCORE: 49324858", 36);
-		scoreText.setFormat("VCR OSD Mono", 32);
+		scoreText.setFormat("VCR OSD Mono", 32, FlxColor.BLUE);
 
 		txtWeekTitle = new FlxText(FlxG.width * 0.7, 10, 0, "", 32);
-		txtWeekTitle.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, RIGHT);
+		txtWeekTitle.setFormat("VCR OSD Mono", 32, FlxColor.BLUE, RIGHT);
 		txtWeekTitle.alpha = 0.7;
 
 		var rankText:FlxText = new FlxText(0, 10);
@@ -166,7 +166,7 @@ class StoryMenuState extends MusicBeatState
 		txtTracklist = new FlxText(FlxG.width * 0.05, tracksSprite.y + 60, 0, "", 32);
 		txtTracklist.alignment = CENTER;
 		txtTracklist.font = rankText.font;
-		txtTracklist.color = 0xFFe55777;
+		txtTracklist.color = 0xFF0000FF;
 		add(txtTracklist);
 		// add(rankText);
 		add(scoreText);
