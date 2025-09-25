8601f87c43d861dbe03128459e1fa800dc9565ab
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-09-25 11:39:57 +0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -99,7 +99,7 @@ class FreeplayState extends MusicBeatState
 		for (i in 0...songs.length)
 		{
 			// var songText:Alphabet = new Alphabet(90, 320, songs[i].songName, true);
-			var songText:Alphabet = new Alphabet(FlxG.width / 2, 300, song[i].songName, true);
+			var songText:Alphabet = new Alphabet(FlxG.width / 2, 300, songs[i].songName, true);
 
 			songText.isMenuItem = true;
 			songText.targetY = i;
